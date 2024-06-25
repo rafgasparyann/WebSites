2967fa866f32f7f2afb85122f8a04d66c41e63ab
RafTechie7 <raf.gasparyan.17@mail.ru>
2024-06-25 18:51:51 +0400

    MySQL-i kpnely

---
+++ ConnectionSQL/src/Main.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/university", "root", "admin");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from student");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getDate(4) + " " + resultSet.getInt(5));
+        }
+    }
+}
