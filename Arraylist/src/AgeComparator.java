863aaefff61dfabdfb9acf0e27ec957d7f123a18
Raf <raf.gasparyan.17@mail.ru>
2024-06-25 19:08:23 +0400

    Delete Arraylist directory

--- Arraylist/src/AgeComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class AgeComparator implements Comparator<Human> {
-    @Override
-    public int compare(Human o1, Human o2) {
-        return o1.getAge() - o2.getAge();
-    }
-}
