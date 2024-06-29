067de8a6b321bc3eeead3205989aac4be44d7967
RafTechie7 <raf.gasparyan.17@mail.ru>
2024-06-29 12:47:39 +0400

    CRUD MySQL

---
+++ CRUDSQL/src/ConnectionDB.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    public static Connection connectionDB() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/university", "root", "admin");
+        System.out.println("Connected to database");
+        return connection;
+    }
+}
