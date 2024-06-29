72df0070ce368199abb83987ed63512b5c6ad520
RafTechie7 <raf.gasparyan.17@mail.ru>
2024-06-29 11:59:21 +0400

    CRUDMySQL

--- ConnectionSQL/src/Main.java
+++ ConnectionSQL/src/Main.java
@@ -53,5 +53,6 @@ public class Main {
 //            System.out.println(resultSet5.getInt(1) + " " + resultSet5.getString(2) + " " + resultSet5.getString(3) + " " + resultSet5.getDate(4) + " " + resultSet5.getInt(5));
 //        }
 
+
     }
 }
