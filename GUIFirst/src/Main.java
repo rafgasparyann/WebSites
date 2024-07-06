7bbe33c2cd60b811deb916a22a30f5ba41c10149
RafTechie7 <raf.gasparyan.17@mail.ru>
2024-07-06 18:05:26 +0400

    First GUI

---
+++ GUIFirst/src/Main.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MyWindow myWindow = new MyWindow();
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
