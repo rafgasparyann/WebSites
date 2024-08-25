51df3e98cce6b975bf63073a3e63650375779922
Raf <raf.gasparyan.17@mail.ru>
2024-08-25 22:10:24 +0400

    Delete GUIFirst directory

--- GUIFirst/src/Main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) {
-        MyWindow myWindow = new MyWindow();
-        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
